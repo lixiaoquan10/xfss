1219445d3deebec79c03e4a4a19db41a7e77b17b
lixiaoquan10 <1943235977@qq.com>
2024-10-24 09:04:14 +0800

    QT5master10.10

---
+++ Monitor/cmyiconstyle.cpp
@@ -0,0 +1,15 @@
+﻿#include "cmyiconstyle.h"
+
+CMyIconStyle::CMyIconStyle() :
+    QCommonStyle()
+{
+}
+
+int CMyIconStyle::pixelMetric(QStyle::PixelMetric metric, const QStyleOption *option, const QWidget *widget) const
+{
+    int n;
+//    int n = QPlastiqueStyle::pixelMetric(metric, option, widget);
+    if(metric == QStyle::PM_SmallIconSize)
+        n = 32;
+    return n;
+}
